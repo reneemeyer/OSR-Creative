fbdcf3cab2b50cb4534b8e36d8773416d0a2da1e
reneedudley <4reneedudley@gmail.com>
2015-12-09 12:15:30 -0600

    took out something

--- js/init.js
+++ js/init.js
@@ -1,10 +1,7 @@
-(function($){
-  $(function(){
-
+$(function(){
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 	$('.collapsible').collapsible({
-      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+      accordion : false
     });
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+}); // end of document ready// end of jQuery name space
