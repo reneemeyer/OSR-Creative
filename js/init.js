013e7ee5b32fbf84717665aebfc0fd56e517f9c2
reneedudley <4reneedudley@gmail.com>
2015-12-09 11:08:43 -0600

    collapsible is a little shit

--- js/init.js
+++ js/init.js
@@ -3,6 +3,8 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-
+	$('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
