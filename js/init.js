196220f171a018d557a9498f4f601a36d4435440
reneedudley <4reneedudley@gmail.com>
2015-12-11 18:14:41 -0600

    arrow fades as you scroll down

--- js/init.js
+++ js/init.js
@@ -2,4 +2,17 @@ $(function(){
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('#arrow').pushpin({ top: $('#arrow').offset().top });
+    var divs = $('#arrow');
+   //  $(window).scroll(function(){
+   // var percent = $(document).scrollTop() / ($(document).height() - $(window).height());
+   // divs.css('opacity', 1 - percent);
+   //  });
+var header = $('#arrow');
+$(window).scroll(function() {
+    header.addClass('lower-opacity');
+    if($(this).scrollTop() === 0) {
+        header.removeClass('lower-opacity');
+    }
+});
+
 }); // end of document ready// end of jQuery name space
