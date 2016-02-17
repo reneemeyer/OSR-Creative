1ebc95fb879f3378c33cdedfddb87c829b3f4e16
reneedudley <4reneedudley@gmail.com>
2016-02-17 10:21:37 -0600

    changed header and logo opacity

--- js/init.js
+++ js/init.js
@@ -9,11 +9,13 @@ $(function(){
      if($(this).scrollTop() === 0) {
         header.removeClass('lower-opacity');
      }
+
     });
     var options = [
     {selector: '#staggered-test', offset: 300, callback: 'Materialize.showStaggeredList("#staggered-test")'  },
     {selector: '#staggered-test2', offset: 350, callback: 'Materialize.showStaggeredList("#staggered-test2")'  }
     ];
     Materialize.scrollFire(options);
+
 
 }); // end of document ready// end of jQuery name space
